8c572d7875a0a3fc63f7bf8427332938f014f415
Logan Ashamallah <loganash500@gmail.com>
2025-06-30 15:57:47 -0700

    Hard code redirect URI, Update client ID, Add owner to app.json

--- config/spotifyAuth.js
+++ config/spotifyAuth.js
@@ -1,11 +1,12 @@
-// config/spotifyAuth.ts
 export const discovery = {
   authorizationEndpoint: "https://accounts.spotify.com/authorize",
   tokenEndpoint: "https://accounts.spotify.com/api/token",
 };
 
-export const SPOTIFY_CLIENT_ID = "your-client-id";
-export const REDIRECT_URI = "exp://127.0.0.1:19000"; // or from AuthSession.makeRedirectUri()
+export const SPOTIFY_CLIENT_ID = "45eb125c13c84eaca7307c1a1097e8af";
+
+export const REDIRECT_URI = "https://auth.expo.io/@loganash/sortify";
+
 export const SCOPES = [
   "user-read-private",
   "playlist-modify-public",
